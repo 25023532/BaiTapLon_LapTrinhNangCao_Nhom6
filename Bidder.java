22460eb7204cedcb667c452847a77d810ec8585b
25023532 <25023532@vnu.edu.vn>
2026-04-26 23:17:04 +0700

    Implement Observer pattern in Bidder class

--- Bidder.java
+++ Bidder.java
@@ -1,6 +1,9 @@
 package org.example.user;
 
-public class Bidder extends User {
+import org.example.model.Bid;
+import org.example.observer.Observer;
+
+public class Bidder extends User implements Observer {
 
     public Bidder(String id, String username, String password) {
         super(id, username, password);
@@ -9,5 +12,10 @@ public class Bidder extends User {
     @Override
     public String getRole() {
         return "BIDDER";
+    }
+
+    @Override
+    public void update(Bid bid) {
+        System.out.println(username + " sees new bid: " + bid.getAmount());
     }
 }
