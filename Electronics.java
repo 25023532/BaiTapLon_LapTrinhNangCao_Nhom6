8a78191910d23469c1b64cb365325c44b8f232c6
25023532 <25023532@vnu.edu.vn>
2026-04-27 21:45:03 +0700

    Update Electronics.java

--- Electronics.java
+++ Electronics.java
@@ -1,13 +1,14 @@
 package org.example.item;
 
+import java.time.LocalDateTime;
+
 public class Electronics extends AuctionItem {
 
-    public Electronics(String id, String name, String desc, double price) {
-        super(id, name, desc, price);
+    public Electronics(String id, String name, String description,
+                       double startPrice, LocalDateTime startTime, LocalDateTime endTime) {
+        super(id, name, description, startPrice, startTime, endTime);
     }
 
     @Override
-    public String getType() {
-        return "Electronics";
-    }
+    public String getType() { return "Electronics"; }
 }
