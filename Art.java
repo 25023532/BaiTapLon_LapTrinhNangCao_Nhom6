d23bcbf24eb3524218d70ac6e775a5024f3522e4
25023532 <25023532@vnu.edu.vn>
2026-04-27 21:44:45 +0700

    Update Art.java

--- Art.java
+++ Art.java
@@ -1,13 +1,14 @@
 package org.example.item;
 
+import java.time.LocalDateTime;
+
 public class Art extends AuctionItem {
 
-    public Art(String id, String name, String desc, double price) {
-        super(id, name, desc, price);
+    public Art(String id, String name, String description,
+               double startPrice, LocalDateTime startTime, LocalDateTime endTime) {
+        super(id, name, description, startPrice, startTime, endTime);
     }
 
     @Override
-    public String getType() {
-        return "Art";
-    }
+    public String getType() { return "Art"; }
 }
