c7c3a50106ad58293d797b7e78d290be070f86e8
25023526 <25023526@vnu.edu.vn>
2026-05-02 18:09:22 +0700

    Feature: Multi-threaded Server running successfully (chay da luong thanh cong)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.nhom6.auctionsystem_nhom6 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.base;
 
     opens com.nhom6.auctionsystem_nhom6 to javafx.fxml;
+    opens com.nhom6.auctionsystem_nhom6.network to javafx.fxml;
+
     exports com.nhom6.auctionsystem_nhom6;
+    exports com.nhom6.auctionsystem_nhom6.network;
 }
