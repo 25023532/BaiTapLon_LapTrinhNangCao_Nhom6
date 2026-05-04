f556d7456bec80fedf5e02c55cb7adb187b0c31c
25023534-NguyenDuyThuy <25023534@vnu.edu.vn>
2026-05-04 17:38:23 +0700

    Update Bidder.java

--- src/main/java/org/example/user/Bidder.java
+++ src/main/java/org/example/user/Bidder.java
@@ -1,1 +1,22 @@
+package org.example.user;
 
+import org.example.model.Bid;
+import org.example.observer.Observer;
+
+public class Bidder extends User implements Observer {
+
+    public Bidder(String id, String username, String password) {
+        super(id, username, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "BIDDER";
+    }
+
+    @Override
+    public void update(Bid bid) {
+        System.out.println("[NOTIFY] " + username + " sees new bid: " + bid.getAmount()
+                + " from " + bid.getBidderId());
+    }
+}
